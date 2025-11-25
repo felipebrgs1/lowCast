2a3c6c7239df9cc021a004b669d0d127ac2805f3
felipeborgaco <felipeborgaco@hotmail.com>
2025-11-25 08:13:53 -0300

    .

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -7,7 +7,7 @@ function ScrollArea({ className, children, ...props }: React.ComponentProps<type
 	return (
 		<ScrollAreaPrimitive.Root
 			data-slot="scroll-area"
-			className={cn("relative", className)}
+			className={cn("relative overflow-hidden", className)}
 			{...props}
 		>
 			<ScrollAreaPrimitive.Viewport
